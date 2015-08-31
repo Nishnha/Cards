446f42e9c185f995d314704377f87081f78f62b0
Nishant Sinha <nishnha@gmail.com>
2015-08-30 22:59:34 -0400

    Added a previous button. Fixed mobile view.

--- cards.js
+++ cards.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
     
     
-    $(".next").click(function() {
+    $(".nextbutton").click(function() {
         $(".active").removeClass("active").addClass("hidden prev");
         $(".prev").next().addClass("active").removeClass("hidden").prev().removeClass("prev");
+    });
+    
+    $(".prevbutton").click(function() {
+        $(".active").removeClass("active").addClass("hidden next");
+        $(".next").prev().addClass("active").removeClass("hidden").next().removeClass("next");
     });
     
     $(".card").click(function(){
